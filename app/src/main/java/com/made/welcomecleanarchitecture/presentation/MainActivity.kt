5afceb4beb42d195aaee769020f0b3eb0729d9bb
Sitifatimah121 <sitifatimah.ft.13@gmail.com>
2023-01-09 21:56:59 +0700

    Update Main Activity

---
+++ app/src/main/java/com/made/welcomecleanarchitecture/presentation/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.made.welcomecleanarchitecture.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.made.welcomecleanarchitecture.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //initialize viewmodel
+        val factory = MainViewModelFactory.getInstance()
+        val viewModel = ViewModelProvider(this, factory)[MainViewModel::class.java]
+
+        viewModel.setName("Mamo")
+        viewModel.message.observe(this, Observer {
+            binding.tvWelcome.text = it.welcomeMessage
+        })
+    }
+}
